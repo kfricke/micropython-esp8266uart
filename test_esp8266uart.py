5049816b1baf43331d5ab7b14a993db82df5d343
kfricke <hashkf@gmail.com>
2015-02-18 22:54:53 +0100

    Test cases implemented for new features.

--- test_esp8266uart.py
+++ test_esp8266uart.py
@@ -61,10 +61,44 @@ for wlan in wlans:
     print("Scanning for WLAN '%s'..." % (wlan['ssid']))
     for wlan2 in esp.list_accesspoints(wlan['ssid']):
         print(wlan2)
-    
+
 print('Setting access point mode...')
 if esp.set_mode(esp8266uart.WIFI_MODES['Access Point + Station']):
     print('Failed!')
 else:
     print('Success!')
 
+print('Reading access point configuration')
+print(esp.get_accesspoint_config())
+print('Listing all stations connected to the module in access point mode...')
+print(esp.list_stations())
+
+print('Checking DHCP client and server settings...')
+for mode in range(3):
+    print(esp.set_dhcp_config(mode, 0))
+    print(esp.set_dhcp_config(mode, 1))
+    print(esp.set_dhcp_config(mode, True))
+    print(esp.set_dhcp_config(mode, False))
+try:
+    print(esp.set_dhcp_config(0, 2))
+except esp8266uart.CommandError:
+    print('Obvious error caught!')
+try:
+    print(esp.set_dhcp_config(4, 1))
+except esp8266uart.CommandError:
+    print('Obvious error caught!')
+
+print('Setting autoconnect to access point in station mode...')
+esp.set_autoconnect(True)
+esp.set_autoconnect(False)
+esp.set_autoconnect(True)
+
+print('Reading and setting the station IP...')
+print(esp.get_station_ip())
+esp.set_station_ip('192.168.1.10')
+print(esp.get_station_ip())
+
+print('Reading and setting the access point IP...')
+print(esp.get_accesspoint_ip())
+esp.set_accesspoint_ip('192.168.1.1')
+print(esp.get_accesspoint_ip())
